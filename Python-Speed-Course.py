08a4e4d86703883e2d1708977ff984a674af1b5b
wuyeelok <wuyeelok@gmail.com>
2021-03-08 10:44:56 +0800

    Basic userinput prompt and print back

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,7 +1,3 @@
-hello = 'tim'
-world = 'world'
-world = hello
-hello = 'no'
-
-print(hello, world)
-print(hello, world)
+name = input('Name: ')
+age = input('Age: ')
+print('Hello', name, 'you are', age, 'years old')
