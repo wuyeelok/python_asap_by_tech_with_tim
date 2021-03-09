581b3bc713cab82fd4e687b5fac28afc38d05334
wuyeelok <wuyeelok@gmail.com>
2021-03-09 10:02:31 +0800

    simple if statement

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,15 +1,6 @@
-x = 7
-y = 8
-z = 0
+x = input('Name: ')
 
-result1 = x == y
-result2 = y > x
-result3 = z < x+2
+if x == 'Ken':
+    print('Yes I am')
 
-print(result1 and result2)
-print(result1 or result2)
-print(not result3)
-print(result3 or not result2 or result1)
-
-# for detail of operator precedence in Python see
-# https://docs.python.org/3/reference/expressions.html#operator-precedence
+print('Always do this')
