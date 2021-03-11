219b5452b4112592f2e30c6d4d3e42bc7e4d7f8a
wuyeelok <wuyeelok@gmail.com>
2021-03-11 15:25:26 +0800

    Basic sliced ops

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,21 +1,6 @@
 myList = [3, "dsd", True, 4]
+myList2 = ['h1', 'hello', 'goodbye', 'cya', 'sure']
+s = "hello"
 
-# Reverse printing of list
-for i in range(len(myList)-1, -1, -1):
-    print(myList[i])
-
-
-for i, ele in enumerate(myList):
-    print(i, ele)
-
-print()
-
-# While loop
-n = 10
-while n > 0:
-    n -= 1
-    if n == 3:
-        break
-    if n % 2 == 1:
-        continue
-    print(n)
+sss = myList2[5:1:-2]
+print(sss)
