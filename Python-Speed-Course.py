25f80ce8d0b9eacbd4787cfd8be890c7492aa195
wuyeelok <wuyeelok@gmail.com>
2021-03-11 11:30:11 +0800

    While loop with break and continue

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -7,3 +7,15 @@ for i in range(len(myList)-1, -1, -1):
 
 for i, ele in enumerate(myList):
     print(i, ele)
+
+print()
+
+# While loop
+n = 10
+while n > 0:
+    n -= 1
+    if n == 3:
+        break
+    if n % 2 == 1:
+        continue
+    print(n)
