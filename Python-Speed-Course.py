a3c76068e804dc3f282d326a1ee0fed0b8089b3f
wuyeelok <wuyeelok@gmail.com>
2021-03-12 15:28:28 +0800

    1st dictionary

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,16 +1,3 @@
-# empty set
-x = set()
-print(type(x))
-
-# empty dictionary
-y = {}
-print(type(y))
-
-# Init set, watch out the diff with dictionary
-z = {2, 4, "sdf", 4}
-print(type(z))
-print(z)
-print('asfweef' in z)
-
-if("sdf" in z):
-    print("the element sdf in in Set z")
+# Dictionary
+myDictionary = {'key': 5}
+print(myDictionary['key'])
