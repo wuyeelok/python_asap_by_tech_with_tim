5cb94cd43966b169793e001fcb0086f4b08773d2
wuyeelok <wuyeelok@gmail.com>
2021-03-12 16:09:38 +0800

    more dictionary example

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -3,7 +3,12 @@ myDictionary = {'key': 5}
 print(myDictionary['key'])
 
 myDictionary['key2'] = 6
-
+myDictionary[9] = [7, 2]
 myDictionary[2] = [3, 5, 6, 9]
 
 print(myDictionary)
+print()
+print(myDictionary.keys())
+
+if 9 in myDictionary:
+    print("It has key 9")
