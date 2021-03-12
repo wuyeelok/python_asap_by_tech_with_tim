295b3702df610a216ef7f9cf11f9142df9fbe765
wuyeelok <wuyeelok@gmail.com>
2021-03-12 16:04:06 +0800

    Dictionary (datatype don't need to be the same)

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,3 +1,9 @@
-# Dictionary
+# Dictionary (datatype don't need to be the same)
 myDictionary = {'key': 5}
 print(myDictionary['key'])
+
+myDictionary['key2'] = 6
+
+myDictionary[2] = [3, 5, 6, 9]
+
+print(myDictionary)
