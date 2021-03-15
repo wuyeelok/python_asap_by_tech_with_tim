9f422a150e4ea44cd10db7966255b7583a7d24e5
wuyeelok <wuyeelok@gmail.com>
2021-03-15 15:31:05 +0800

    List Comprehension vs For Loop in Python

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,18 +1,10 @@
-# Dictionary (datatype don't need to be the same)
-myDictionary = {'key': 5}
-print(myDictionary['key'])
-
-myDictionary['key2'] = 6
-myDictionary[9] = [7, 2]
-myDictionary[2] = [3, 5, 6, 9]
-
-print(myDictionary)
-print()
-print(myDictionary.keys())
-
-if 9 in myDictionary:
-    print("It has key 9")
+# Python - List Comprehension
+myList = [item for item in range(10)]
+print("myList: {}".format(myList))
 
 
-for key, value in myDictionary.items():
-    print(key, value)
+# Python - normal for loop
+myList2 = []
+for item in range(10):
+    myList2.append(item)
+print("myList2: {}".format(myList2))
