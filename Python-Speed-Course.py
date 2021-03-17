15398428ddc474fc8b78c5f4b1884dd1b7e54d1a
wuyeelok <wuyeelok@gmail.com>
2021-03-17 11:13:29 +0800

    1st funciton

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,22 +1,5 @@
-# Python - List Comprehension
-myList = [item for item in range(10)]
-print("myList: {}".format(myList))
+def myFunc(x, y):
+    return x * y
 
 
-# Python - normal for loop
-myList2 = []
-for item in range(10):
-    myList2.append(item)
-print("myList2: {}".format(myList2))
-
-
-# List Comprehesion can also turn string into list
-myNameList = [letter for letter in 'Kenneth']
-print("myNameList is {}".format(myNameList))
-
-
-x = [[x for x in range(4)] for x in range(5)]
-print(x)
-
-y = tuple(i for i in range(100) if i % 5 == 0)
-print(y)
+print(myFunc(6, 7))
