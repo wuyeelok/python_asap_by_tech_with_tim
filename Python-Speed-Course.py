1bbee37d407218768a26a58279da9c10157f0924
wuyeelok <wuyeelok@gmail.com>
2021-03-17 11:06:34 +0800

    more example

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -13,3 +13,10 @@ print("myList2: {}".format(myList2))
 # List Comprehesion can also turn string into list
 myNameList = [letter for letter in 'Kenneth']
 print("myNameList is {}".format(myNameList))
+
+
+x = [[x for x in range(4)] for x in range(5)]
+print(x)
+
+y = tuple(i for i in range(100) if i % 5 == 0)
+print(y)
