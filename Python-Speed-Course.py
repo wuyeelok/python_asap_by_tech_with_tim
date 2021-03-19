979c2ef1021cc65823c0e91ddd5e1de0ab35ae95
wuyeelok <wuyeelok@gmail.com>
2021-03-19 16:00:10 +0800

    simple try except finally

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,28 +1,6 @@
-# mutiply args
-def my_mutiply(*numbers):
-    result = 1
-    for num in numbers:
-        result *= num
-    return result
-
-
-print(my_mutiply(4, 2, 5))
-
-# Use start when calling and passing mutiple argument
-
-
-def myFunc(x, y):
-    print(x, y)
-
-
-pairs = [(1, 2), (3, 4)]
-
-for pair in pairs:
-    myFunc(*pair)
-
-
-def yourFunc(*args, **kwargs):
-    print(args, kwargs)
-
-
-yourFunc(4, 2, 1, one=0, tow=1)
+try:
+    x = 7 / 0
+except Exception as e:
+    print(e)
+finally:
+    print('finally')
