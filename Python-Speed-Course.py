5db2e2b45ae306d69423a4222b7e19ac51438edf
wuyeelok <wuyeelok@gmail.com>
2021-03-22 16:30:09 +0800

    map and filter and lambda function

--- Python-Speed-Course.py
+++ Python-Speed-Course.py
@@ -1,8 +1,11 @@
-try:
-    x = 7 / 5
-except Exception as e:
-    print(e)
-else:
-    print("Nothing went wrong")
-finally:
-    print('finally')
+myList = [4, 6, 23, 34, 434, 2344, 24453, 1]
+
+myNewList = list(map(lambda i: i+2, myList))
+
+myNewList2 = list(filter(lambda i: i % 2 == 0, myList))
+
+print(myList)
+print()
+print(myNewList)
+print()
+print(myNewList2)
